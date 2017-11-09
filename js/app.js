1956de813d94eae5b6fd35cfd650f50499aa65fb
Shan Rizvi <shanrizvi@Shans-MacBook-Pro.local>
2017-11-09 09:48:48 +0300

    dkfjd

--- js/app.js
+++ js/app.js
@@ -5,11 +5,11 @@ function main() {
     setTimeout(function(){ $('.theHeader').fadeIn(2500); }, 1300);
 	setTimeout(function(){ $('.desc').fadeIn(3500); }, 2000);
 	setTimeout(function(){ $('.action').fadeIn(4000); }, 3000);
-	window.sr = ScrollReveal();
-	sr.reveal('.image', { duration: 3000 }, 600);
-	sr.reveal('.hi', { duration: 6000 }, 1200);
-	sr.reveal('.why', { duration: 6000 }, 1200);
-	sr.reveal('.contact', { duration: 6000 }, 1200);
-
+	
 }
 $(document).ready(main);
+window.sr = ScrollReveal();
+sr.reveal('.image', { duration: 3000 }, 600);
+sr.reveal('.hi', { duration: 6000 }, 1200);
+sr.reveal('.why', { duration: 6000 }, 1200);
+sr.reveal('.contact', { duration: 6000 }, 1200);
